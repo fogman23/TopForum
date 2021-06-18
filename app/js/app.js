68e5fa304328fb108f0976a01177463ebe957747
Valera <fogman23@gmail.com>
2021-06-18 09:19:35 +0500

    Адаптация главной страницы

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 	$('.promo__wrapper').slick({
+		autoplay: true,
 		speed: 500,
-		fade: true,
-		cssEase: 'linear',
 		slidesToShow: 1,
+		slidesToScroll: 1,
 		asNavFor: '.promo__bg',
+		responsive: [{
+			breakpoint: 575,
+			settings: {
+				arrows: false,
+			}
+		}]
 	});
 
 	$('.promo__bg').slick({
+		autoplay: true,
 		arrows: false,
 		speed: 500,
 		slidesToShow: 1,
+		slidesToScroll: 1,
 		asNavFor: '.promo__wrapper',
 	});
 
 	$('.reviews__slider').slick({
+		autoplay: true,
 		speed: 500,
 		slidesToShow: 2,
 		slidesToScroll: 1,
@@ -30,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 
 	$('.clients__slider').slick({
+		autoplay: true,
 		speed: 500,
 		slidesToShow: 6,
 		slidesToScroll: 1,
@@ -51,18 +61,45 @@ document.addEventListener("DOMContentLoaded", function () {
 			{
 				breakpoint: 768,
 				settings: {
+					arrows: false,
 					slidesToShow: 3,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 575,
+				settings: {
+					arrows: false,
+					slidesToShow: 2,
 					slidesToScroll: 1,
 				}
 			}
 		]
 	});
 
-	$('[data-modal=subscribe]').on('click', function () {
+	$('[data-modal=subscribe]').on('click', function (e) {
+		e.preventDefault();
 		$('.overlay, #subscribe').fadeIn('slow');
 	});
 	$('.modal__close').on('click', function () {
 		$('.overlay, #subscribe').fadeOut('slow');
 	});
+
+	$('.hamburger').click(function () {
+		$(this).toggleClass('hamburger_active');
+		$('.menu').slideToggle(400);
+	});
+
+	if ($(window).width() > 768) {
+		$('.menu').find('> li').hover(function () {
+			$('.menu > li').not(this).find('ul').slideUp;
+			$(this).find('ul').stop(true, true).slideToggle(400);
+		});
+	} else {
+		$('.menu').find('> li').click(function () {
+			$('.menu > li').not(this).find('ul').slideUp;
+			$(this).find('ul').stop(true, true).slideToggle(400);
+		});
+	}
 
 });
